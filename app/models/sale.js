5e0f1c05d187adedebb4f77fed43518f1a5d87dd
Greg Babiars <gbabiars@gmail.com>
2014-08-18 20:51:25 -0700

    Load sales and display basic table

--- app/models/sale.js
+++ app/models/sale.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    volume: DS.attr('number'),
+    price: DS.attr('number'),
+    date: DS.attr('date'),
+    product: DS.belongsTo('product')
 });
